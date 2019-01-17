e0627edfbd947b1c0855771329aa4b8506c6361e
Armughan-Shahid <marmughanshahid@gmail.com>
2019-01-17 17:15:09 +0500

    changing debugging.sh

--- samples/coco/debugging.sh
+++ samples/coco/debugging.sh
@@ -2,4 +2,4 @@
 cd ../..
 git pull
 cd samples/coco/
-python3 debugcoco.py train --dataset=/home/dense_pose1/coco --model=last
+python3 debugcoco.py train --dataset=/home/dense_pose1/cocoimages --model=last
